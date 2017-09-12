1597bc9a27259c6090d82de0eccc9c5895903eee
willy <willy@10.0.0.23>
2017-09-11 22:16:16 -0600

    commit 2, onto last dealio but may need assistance finding 1 in for loop

--- array me/src/arraysarelotsoffun.java
+++ array me/src/arraysarelotsoffun.java
@@ -1,38 +1,68 @@
 import java.util.Scanner;
 
-public class arraysarelotsoffun
+public class arraysarelotsoffun 
+{
+	 
+	
+	
+	public static void main(String[] args) 
 	{
+	/*	Scanner favoriteSweets = new Scanner(System.in);
 
-		public static void main(String[] args)
-			{
-				Scanner favoriteSweets = new Scanner(System.in);
-				
-				System.out.println("How many sweets do you enjoy?");
-				int numberOfSweets = favoriteSweets.nextInt();
-				System.out.println("What sweets do you enjoy?");
-				
-				Scanner line = new Scanner(System.in);
-				String[] array = new String[numberOfSweets];
-				String c = line.nextLine();
-				
-				
-				for (int i = 1; i <= numberOfSweets; i++) 
-				{
-					Scanner sweets = new Scanner(System.in);
-					System.out.println("And?");
-					String favsweets = sweets.nextLine();
-					
-					array[i - 1] = favsweets;
-					
-					
-				}
-				for (int j = 0; j < numberOfSweets; j++) 
-				{
-					
-			System.out.println("Your fav sweets are " + array[j]);
-				}
-				
-				
-			}
+		System.out.println("How many sweets do you enjoy?");
+		
+		int numberOfSweets = favoriteSweets.nextInt();
+		
+		String[] array = new String[numberOfSweets];
+		
+		System.out.println("What sweets are your top choices?");
+		
+		
+		for (int i = 1; i <= numberOfSweets; i++) 
+		{
 
+			Scanner sweets = new Scanner(System.in);
+			String favsweets = sweets.nextLine();
+
+			array[i - 1] = favsweets;
+			
+
+		}
+		for (int j = 0; j < numberOfSweets; j++) 
+		{
+
+			System.out.println("Your top picks for sweets would be: " + array[j]);
+		}
+      */
+		//odd or even 
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter 8 odd or even numbers");
+		int userInput = s.nextInt();
+		
+		int numbers[] = new int[8];
+		
+        for (int i = 1; i < 8; i++) 
+        {
+            numbers[i] = s.nextInt();
+        }
+        System.out.print("Odds: ");
+        for(int i = 1 ; i < 8 ; i++)
+        {
+            if(numbers[i] % 2 != 0)
+            {
+                System.out.print(numbers[i]+" ");
+                
+            }
+            
+        }
+        System.out.println(" ");
+        System.out.print("Evens: ");
+        for(int i = 1; i < 8; i++) {
+        	if(numbers[i] % 2 == 0) {
+        		System.out.print(numbers[i]+" ");
+        	}
+        }
+		
 	}
+
+}
